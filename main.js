6a486be37fac4fc96bbb521901a5e92bdca046ea
Dewald Fourie <dewaldfourie0808@gmail.com>
2023-05-16 16:19:01 +0200

    added button styling and more logic

--- main.js
+++ main.js
@@ -7,9 +7,14 @@ let $curTime = document.getElementById("time");
 let $temp = document.getElementById("temp");
 let $conditions = document.getElementById("conditions");
 let $icon = document.getElementById("icon");
+let $windSpeed = document.getElementById("windSpeed");
+let $windDir = document.getElementById("windDir");
+let $lastUpdate = document.getElementById("update");
 let $location_input = document.getElementById("location-in");
 let $searchBtn = document.querySelector(".searchBtn");
 let $toggle = document.getElementById("toggle");
+let $refreshBtn = document.querySelector(".refreshBtn");
+
 
 function setWeatherData(data){
     $location.textContent = data.location;
@@ -18,7 +23,10 @@ function setWeatherData(data){
     $curTime.textContent = data.localTime.split(" ")[1];
     $temp.textContent = data.tempC + " °C";
     $conditions.textContent = data.condition;
+    $windSpeed.textContent = data.windKph + " km/h"
+    $windDir.textContent = data.windDir
     $icon.src = data.icon;
+    $lastUpdate.textContent = data.lastUpdate
     if (data.isDay === 1){
         if(data.code === 1000){
             console.log("sunny");
@@ -33,11 +41,13 @@ function setWeatherData(data){
     $toggle.addEventListener("click", () => {
         if($toggle.checked) {
             $temp.textContent = data.tempF + " °F";
+            $windSpeed.textContent = data.windMph + " mph"
         }
         else{
             $temp.textContent = data.tempC + " °C";
+            $windSpeed.textContent = data.windKph + " km/h"
         }
-    })   
+    }) 
 }
 
 async function getWeatherData(location){
@@ -57,14 +67,19 @@ async function getWeatherData(location){
             condition: data.current.condition.text,
             icon: data.current.condition.icon,
             code: data.current.condition.code,
-            isDay: data.current.is_day
+            isDay: data.current.is_day,
+            windKph: data.current.wind_kph,
+            windMph: data.current.wind_mph,
+            windDir: data.current.wind_dir
         }
+        localStorage.setItem("lastLocation", location);
         return weatherData;
     } 
     catch (error) {
         console.error("Not Found", error)
     }
 }
+
 
 $searchBtn.addEventListener("click", () => {
     getWeatherData($location_input.value).then(weatherData => {
@@ -73,8 +88,23 @@ $searchBtn.addEventListener("click", () => {
     $location_input.value = "";
 });
 
+$refreshBtn.addEventListener("click", () => {
+    const lastLocation = localStorage.getItem("lastLocation");
+    if (lastLocation) {
+        getWeatherData(lastLocation).then(weatherData => {
+            setWeatherData(weatherData);
+        })
+        .catch(error => {
+            console.error("Error refreshing weather data:", error)
+        })
+        alert("Data Refreshed");
+    }
+});
+
+
+
 getWeatherData("Dublin").then(weatherData => {
     console.log(weatherData);
     setWeatherData(weatherData);
-})
+});
 
